1655016fc3b68d140e520050bfc7acbb740a4fc1
onsoim <onsoim@gmail.com>
2021-08-01 02:40:19 +0900

    [UPDATE] code run from training to testing

--- src/runner.ps1
+++ src/runner.ps1
@@ -2,6 +2,7 @@ foreach ($model in 'seq2seq', 'attention', 'transformer')
 {
     foreach ($format in 'AVIF', 'BMP', 'H264')
     {
+        python .\ddrfuzz.py --mode train --epochs 30 --model $model --format $format --path ..\seeds\$format\1000\sampling\
         python .\ddrfuzz.py --mode test --model $model --format $format --path ..\seeds\AVIF\1000\sampling\
     }
 }
